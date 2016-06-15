afb6461125f8465dafa2328593d93782a517a034
Simona Banut <banutika02@gmail.com>
2016-06-15 22:59:43 +0300

    tema 2 tabele users si agendas / un user are mai multe date in agenda/ CRUD combinat

--- src/JDBCTest.java
+++ src/JDBCTest.java
@@ -1,56 +1,77 @@
 import java.sql.*;
-import java.util.Objects;
-import java.util.Scanner;
 import java.lang.String;
 
-public class JDBCTest {
-
+public class JDBCTest extends read_print {
     public static void main(String[] args) {
         System.out.println("Hello database users! We are going to call DB");
-        try {
-//            demoCreate();
-            String nuser = readStringConsole("userul pentru care doriti aduagare in agenda ");
-            demoCreateAG(nuser);
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-    }
-        private static void demoCreate() throws ClassNotFoundException, SQLException {
+        int optiune = 0;
 
-            // 1. load driver class.forname-incarc driverul
-            Class.forName("org.postgresql.Driver");
+        do {
+            printConsole("Meniul principal:");
+            printConsole("1 - listare 'users'/ 2 - Creare date in user / 3 - Cautare user /");
+            printConsole (" 4 - adaugare date agenda pentru un user / 5 - Stergere user /6-exit");
+            printConsole("- * - * - * - * - * - * - * - *- * - * - * - * - -* - *-");
 
-            // 2. define connection params to db
-            final String URL = "jdbc:postgresql://54.93.65.5:5432/4_Simona";
-            final String USERNAME = "fasttrackit_dev";
-            final String PASSWORD = "fasttrackit_dev";
+            optiune = readIntConsole("Alegeti optiunea dorita:");
+            System.out.println();
 
-            // 3. obtain a connection
-            Connection conn = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+            if (optiune == 1) {
+                try {
+                    demoRead();
+                    System.out.println();
+                } catch (ClassNotFoundException e) {
+                    e.printStackTrace();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            } else if (optiune == 2) {
 
-            // 4. create a query statement
-            Boolean k;
-            PreparedStatement pSt = conn.prepareStatement("INSERT INTO users (nume, parola) VALUES (?,?)");
-            while (k=true) {
-                String n = readStringConsole("userul: ");
-                String p = readStringConsole("parola: ");
-                pSt.setString(1, n);
-                pSt.setString(2, p);
+                try {
+                    demoCreate();
+                } catch (ClassNotFoundException e) {
+                    e.printStackTrace();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            } else if (optiune == 3) {
+                String nume = readStringConsole("Introduceti numele cautat: ");
+                try {
+                    demoSearch(nume);
+                } catch (ClassNotFoundException e) {
+                    e.printStackTrace();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            } else if (optiune == 4) {
+                String nume = readStringConsole("Introduceti userul pentru care vreti aduagare date in agenda:");
+                String numeNou = readStringConsole("Introduceti numele agenda:");
+                String prenumeNou = readStringConsole("Introduceti prenume agenda");
+                String telefonNou = readStringConsole("Introduceti numarul de telefon:");
+                try {
+                    demoCreateAG(nume, numeNou, prenumeNou, telefonNou);
+                } catch (ClassNotFoundException e) {
+                    e.printStackTrace();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            } else if (optiune == 5) {
+                String nume = readStringConsole("ce user vreti sa stergeti:");
 
-                // 5. execute a prepared statement
-                int rowsInserted = pSt.executeUpdate();
-                int kk = readIntConsole("mai doriti sa adaugati useri? 1/0: ");
-                if (kk==1) {k=false;}
+                try {
+                    demoDelete(nume);
+                } catch (ClassNotFoundException e) {
+                    e.printStackTrace();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
             }
 
-            // 6. close the objects
-            pSt.close();
-            conn.close();
+            }
+            while (optiune != 6) ;
+
         }
-//adaugare date in Agenda
-    private static void demoCreateAG(String nuser) throws ClassNotFoundException, SQLException {
+
+    private static void demoCreate() throws ClassNotFoundException, SQLException {
 
         // 1. load driver class.forname-incarc driverul
         Class.forName("org.postgresql.Driver");
@@ -62,45 +83,147 @@ public class JDBCTest {
 
         // 3. obtain a connection
         Connection conn = DriverManager.getConnection(URL, USERNAME, PASSWORD);
-        // 3.1. cauta userul in users
-        Statement st = conn.createStatement();
-         System.out.println("userul pentru care doriti adaugare date este: " + nuser);
 
-        ResultSet rs = st.executeQuery("Select idu FROM users where nume='nuser'");
-        String aidi = rs.getString("idu");
-        System.out.println("are ID: " + aidi);
+        // 4. create a query statement
+        Boolean k = true;
+        PreparedStatement pSt = conn.prepareStatement("INSERT INTO users (nume, parola) VALUES (?,?)");
+        while (k) {
+            String n = readStringConsole("userul: ");
+            String p = readStringConsole("parola: ");
+            pSt.setString(1, n);
+            pSt.setString(2, p);
+
+            // 5. execute a prepared statement
+            int rowsInserted = pSt.executeUpdate();
+            String kk = readStringConsole("mai doriti sa adaugati useri? Y/N: ");
+            if (kk.equals("N")) {
+                k = false;
+            }
+        }
+        // 6. close the objects
+        pSt.close();
+        conn.close();
+    }
+
+    //adaugare date in Agenda
+    private static void demoCreateAG(String nuser, String numeNou, String prenumeNou, String telefonNou) throws ClassNotFoundException, SQLException {
+
+        // 1. load driver class.forname-incarc driverul
+        Class.forName("org.postgresql.Driver");
+
+        // 2. define connection params to db
+        final String URL = "jdbc:postgresql://54.93.65.5:5432/4_Simona";
+        final String USERNAME = "fasttrackit_dev";
+        final String PASSWORD = "fasttrackit_dev";
+
+        // 3. obtain a connection
+        Connection conn = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+
+        // 3.1. cauta userul in tabela users
+        Statement st = conn.createStatement();
+        System.out.println("adaugam date pentru :  " + nuser);
+        ResultSet rs = st.executeQuery("Select idu FROM users where nume='" + nuser + "'");
+         int aidi=0;
+        // vezi ID , campul idu in tabela users
+        while (rs.next()) {
+            aidi = rs.getInt("idu");
+            System.out.println("are ID: " + aidi);}
 
         // 4. adaugare date in agenda pentru un user anume
         PreparedStatement pSt = conn.prepareStatement("INSERT INTO agendas (idu, nume, prenume, telefon) VALUES (?,?,?,?)");
-        pSt.setString(1, aidi);
-            String nn = readStringConsole("nume: ");
-            String pp = readStringConsole("prenume: ");
-            String t= readStringConsole("nr telefon: ");
-            pSt.setString(2, nn);
-            pSt.setString(3, pp);
-            pSt.setString(4, t);
-
-
-
-//            // 5. execute a prepared statement
-//            int rowsInserted = pSt.executeUpdate();
-
-
-        // 6. close the objects
+        pSt.setInt(1, aidi);
+        pSt.setString(2, numeNou);
+        pSt.setString(3, prenumeNou);
+        pSt.setString(4, telefonNou);
+        // 5. execute a prepared statement
+        int rowsInserted = pSt.executeUpdate();
+        //6. close the objects
         rs.close();
         st.close();
         conn.close();
     }
 
+    private static void demoRead() throws ClassNotFoundException, SQLException {
 
-    public static String readStringConsole(String label) {
-        System.out.print(label);
-        String input = new Scanner(System.in).nextLine();
-        return input;
+        // 1. load driver class.forname-incarc driverul
+        Class.forName("org.postgresql.Driver");
+        // 2. define connection params to db
+        final String URL = "jdbc:postgresql://54.93.65.5:5432/4_Simona";
+        final String USERNAME = "fasttrackit_dev";
+        final String PASSWORD = "fasttrackit_dev";
+        // 3. obtain a connection
+        Connection conn = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+        // 4. create a query statement
+        Statement st = conn.createStatement();
+
+        // 5. execute a query
+        ResultSet rs = st.executeQuery("SELECT nume,parola FROM users");
+
+        // 6. iterate the result set and print the values
+        while (rs.next()) {
+            System.out.print(rs.getString("nume").trim());
+            System.out.print("---");
+            System.out.println(rs.getString("parola").trim());
+        }
+        // 7. close the objects
+        rs.close();
+        st.close();
+        conn.close();
+
     }
-    public static int readIntConsole(String label) {
-        System.out.print(label);
-        int input = new Scanner(System.in).nextInt();
-        return input;
+
+    private static void demoSearch(String n) throws ClassNotFoundException, SQLException {
+
+        // 1. load driver class.forname-incarc driverul
+        Class.forName("org.postgresql.Driver");
+        // 2. define connection params to db
+        final String URL = "jdbc:postgresql://54.93.65.5:5432/4_Simona";
+        final String USERNAME = "fasttrackit_dev";
+        final String PASSWORD = "fasttrackit_dev";
+        // 3. obtain a connection
+        Connection conn = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+        // 4. create a query statement
+        Statement st = conn.createStatement();
+
+        // 5. execute a query
+        ResultSet rs = st.executeQuery("SELECT nume,parola FROM users WHERE nume='" + n + "'");
+
+        if (!rs.isBeforeFirst()) {
+            System.out.println("Numele nu este in agenda");
+        }
+        // 6. iterate the result set and print the values
+        while (rs.next()) {
+            System.out.println("numele cautat se afla in users cu datele");
+            System.out.print(rs.getString("nume").trim());
+            System.out.print("---");
+            System.out.println(rs.getString("parola").trim());
+        }
+        // 7. close the objects
+        rs.close();
+        st.close();
+        conn.close();
+
     }
+    private static void demoDelete(String num) throws ClassNotFoundException, SQLException {
+
+        // 1. load driver class.forname-incarc driverul
+        Class.forName("org.postgresql.Driver");
+        // 2. define connection params to db
+        final String URL = "jdbc:postgresql://54.93.65.5:5432/4_Simona";
+        final String USERNAME = "fasttrackit_dev";
+        final String PASSWORD = "fasttrackit_dev";
+        // 3. obtain a connection
+        Connection conn = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+        // 4. create a query statement
+        PreparedStatement pSt = conn.prepareStatement("DELETE FROM users WHERE nume='" + num + "'");
+        // 5. execute a prepared statement
+        int rowsD = pSt.executeUpdate();
+
+        System.out.println(rowsD + " linie stearsa");
+
+        // 6. close the objects
+        pSt.close();
+        conn.close();
+    }
+
 }
